e7fc058fe66f7192bb429f493610dfa33cd45f87
Eyup Gurel <eyupgurel@gmail.com>
2019-01-06 21:40:43 +0300

    kotlin upgrade

---
+++ app/src/main/java/com/alienstar/cyrus/advancedandroid/base/ActivityBindingModule.kt
@@ -0,0 +1,23 @@
+package com.alienstar.cyrus.advancedandroid.base
+
+import android.app.Activity
+
+import com.alienstar.cyrus.advancedandroid.home.MainActivity
+import com.alienstar.cyrus.advancedandroid.home.MainActivityComponent
+
+import dagger.Binds
+import dagger.Module
+import dagger.android.ActivityKey
+import dagger.android.AndroidInjector
+import dagger.multibindings.IntoMap
+
+/**
+ * Created by cyrus on 3/4/18.
+ */
+@Module(subcomponents = arrayOf(MainActivityComponent::class))
+abstract class ActivityBindingModule {
+    @Binds
+    @IntoMap
+    @ActivityKey(MainActivity::class)
+    internal abstract fun provideMainActivityInjector(builder: MainActivityComponent.Builder): AndroidInjector.Factory<out Activity>
+}
