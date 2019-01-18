3a41b4ca903e409446e147bebe72f7d6a66b2525
Eyup Gurel <eyupgurel@gmail.com>
2019-01-18 16:24:13 +0300

    upgrade

---
+++ app/src/main/java/com/alienstar/cyrus/advancedandroid/trending/TrendingReposComponent.kt
@@ -0,0 +1,18 @@
+package com.alienstar.cyrus.advancedandroid.trending
+
+import com.alienstar.cyrus.advancedandroid.base.ScreenModule
+import com.alienstar.cyrus.advancedandroid.di.ScreenComponent
+import com.alienstar.cyrus.advancedandroid.di.ScreenScope
+
+import dagger.Subcomponent
+import dagger.android.AndroidInjector
+
+/**
+ * Created by cyrus on 3/5/18.
+ */
+@ScreenScope
+@Subcomponent(modules = arrayOf(ScreenModule::class, TrendingReposScreenModule::class))
+interface TrendingReposComponent : ScreenComponent<TrendingReposController> {
+    @Subcomponent.Builder
+    abstract class Builder : AndroidInjector.Builder<TrendingReposController>()
+}
