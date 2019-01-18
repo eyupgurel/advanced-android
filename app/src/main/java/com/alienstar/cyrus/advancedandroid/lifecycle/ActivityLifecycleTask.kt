220347fc1cfce0db97a18ccab8906d0d8c2d8ef5
Eyup Gurel <eyupgurel@gmail.com>
2019-01-18 16:17:28 +0300

    upgrade

---
+++ app/src/main/java/com/alienstar/cyrus/advancedandroid/lifecycle/ActivityLifecycleTask.kt
@@ -0,0 +1,36 @@
+package com.alienstar.cyrus.advancedandroid.lifecycle
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by cyrus on 3/17/18.
+ */
+
+abstract class ActivityLifecycleTask {
+
+    open fun onCreate(activity: AppCompatActivity) {
+
+    }
+
+    fun onStart(activity: AppCompatActivity) {
+
+    }
+
+    fun onResume(activity: AppCompatActivity) {
+
+    }
+
+    fun onPause(activity: AppCompatActivity) {
+
+    }
+
+    fun onStop(activity: AppCompatActivity) {
+
+    }
+
+    open fun onDestroy(activity: AppCompatActivity) {
+
+    }
+
+
+}
